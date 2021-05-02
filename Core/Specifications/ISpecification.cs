f39bec31436e4d3e27bc98c48331d1b77815ec51
Will Reichl <willreichl@gmail.com>
2021-05-02 07:59:11 -0400

    S4L37 - API Generic Repository - Creating Specification Evaluator
    * Does not handle ThenIncludes scenario, but this is not a requirement for this project

---
+++ Core/Specifications/ISpecification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public interface ISpecification<T>
+    {
+        /// <summary>
+        /// Where criteria 
+        /// </summary>
+        Expression<Func<T, bool>> Criteria { get; }
+
+        /// <summary>
+        /// Includes operations
+        /// </summary>
+        List<Expression<Func<T, object>>> Includes { get; }
+    }
+}
---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly DbContext _context;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
