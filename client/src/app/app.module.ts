5cf13c17038ea4b5af52aaa7b0d07b7ab615994d
Will Reichl <willreichl@gmail.com>
2021-05-20 21:34:38 -0400

    End of Section 11 - Client Error Handling

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CoreModule} from './core/core.module';
+import {ErrorInterceptor} from './core/interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import {CoreModule} from './core/core.module';
     HttpClientModule,
     CoreModule,
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
