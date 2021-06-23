b00db2a25cc2bb265f10475392b6eb86215d159d
Will Reichl <willreichl@gmail.com>
2021-06-23 07:29:54 -0400

    Section 18 - API - Orders - Part 4

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -21,6 +21,7 @@ namespace API.Helpers
                 .ReverseMap();
             CreateMap<CustomerBasketDto, CustomerBasket>();
             CreateMap<BasketItemDto, BasketItem>();
+            CreateMap<AddressDto, Core.Entities.OrderAggregate.Address>();
         }
     }
 }
